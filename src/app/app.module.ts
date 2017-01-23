bafb29e6446f95523e88a4094bd231ed6972c197
YaroslavPr <yarosalvp@softwareplanet.uk.com>
2017-01-24 01:00:58 +0200

    insert head-bar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
-
 import {SlidePanel} from './s-panel';
-
 import {MyHeader} from './my-header';
 import {MyFlexContainer} from './my-flex-container';
 import {MyFooter} from './my-footer';
@@ -14,18 +11,18 @@ import {InfoBlock} from './info-block';
 import {BigImage} from './big-image';
 import {ArtInformations} from './art.informations';
 import {ArtService} from './art.service'
+import { LoginField } from './app.login-field'
  
 @NgModule({
   declarations: [
-    AppComponent, SlidePanel, MyHeader, MyFooter, MyFlexContainer, InfoBlock, BigImage, ArtInformations
+    AppComponent, SlidePanel, MyHeader, MyFooter, MyFlexContainer, InfoBlock, BigImage, ArtInformations, LoginField
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-  
   ],
   providers: [ArtService],
-  bootstrap: [AppComponent, SlidePanel, MyHeader, MyFooter, MyFlexContainer, InfoBlock, BigImage, ArtInformations]
+  bootstrap: [AppComponent, SlidePanel, MyHeader, MyFooter, MyFlexContainer, InfoBlock, BigImage, ArtInformations, LoginField]
 })
 export class AppModule { }
--- src/app/my-header.ts
+++ src/app/my-header.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, trigger, state, style, transition, animate  } from '@angular/core';
 
 @Component({
   selector: 'my-header',
   templateUrl: 'my-header.html',
-  styleUrls: ['my-header.css']
+  styleUrls: ['my-header.css'],
+  animations: [
+    trigger('slideInOut', [
+      state('in', style({
+        transform: 'translateY(25px)',
+        opacity: 1
+      })),
+      state('out', style({
+        transform: 'translateY(-25px)',
+        opacity: 0
+      })),
+      transition('in => out', animate('250ms ease-in-out')),
+      transition('out => in', animate('250ms ease-in-out'))
+    ])]
 })
 
 
 export class MyHeader {
-  
+  btnState:string = 'out';
+  searchKeyWord:string = "";
+  isLogFieldActive = false;
+  //arts: Art[] = [];
+  toggleLoginField():void{
+        if(this.btnState === 'out') {
+          this.btnState = 'in';
+          this.isLogFieldActive = true;
+        }
+        else {
+          this.btnState = "out";
+          this.isLogFieldActive = false;
+        }
+    }
 }
 
